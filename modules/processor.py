a431fb53519bf2faf92a2e9e4cebc148f9ebdd8a
oksanaaam <ksyusha0kubay@gmail.com>
2023-10-12 13:09:44 +0300

    Set initial help button logic

--- modules/processor.py
+++ modules/processor.py
@@ -24,7 +24,7 @@ form_router = Router()
 
 
 class Processor(StatesGroup):
-    chat_bot_type = State()
+    chat_bot_model = State()
     is_with_memory = State()
     is_with_context = State()
     is_with_internet_access = State()
@@ -38,9 +38,9 @@ class Processor(StatesGroup):
 
 @form_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
-    await state.set_state(Processor.chat_bot_type)
+    await state.set_state(Processor.chat_bot_model)
     await message.answer(
-        f"Please select chatbot type.",
+        f"Hi, I am a ContextGPT bot👋 \nPlease select chatbot type:",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[
                 [
@@ -53,9 +53,24 @@ async def command_start(message: Message, state: FSMContext) -> None:
     )
 
 
-@form_router.message(Processor.chat_bot_type, F.text.in_({"GPT-3.5", "GPT-4"}))
-async def process_chat_bot_type(message: Message, state: FSMContext) -> None:
-    await state.update_data(chat_bot_type=message.text)
+@form_router.message(Command("help"))
+async def command_help(message: Message):
+    help_message = """
+    Here are the available commands:
+    /start - start the bot
+    /reset - reset the context
+    /cancel - cancel the current action
+    /uploadPDF - upload new PDF file
+    /sendURL - send new URL
+    
+    /help - show this help message
+    """
+    await message.answer(help_message, parse_mode=ParseMode.MARKDOWN)
+
+
+@form_router.message(Processor.chat_bot_model, F.text.in_({"GPT-3.5", "GPT-4"}))
+async def process_chat_bot_model(message: Message, state: FSMContext) -> None:
+    await state.update_data(chat_bot_model=message.text)
     await state.set_state(Processor.is_with_memory)
     await message.answer(
         f"Would you like to use memory?",
@@ -136,7 +151,7 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     )
 
 
-@form_router.message(Processor.chat_bot_type)
+@form_router.message(Processor.chat_bot_model)
 async def process_unknown_write_bots(message: Message) -> None:
     await message.reply("I don't understand you :(")
 
@@ -159,18 +174,54 @@ async def process_unknown_write_bots(message: Message) -> None:
 async def show_summary(
     message: Message, data: Dict[str, Any], positive: bool = True
 ) -> None:
-    chat_bot_type = data.get("chat_bot_typ", "GPT-3.5")
+    chat_bot_model = data.get("chat_bot_model", "GPT-3.5")
     is_with_memory = data.get("is_with_memory", "Yes")
     is_with_context = data.get("is_with_context", "Yes")
     is_with_internet_access = data.get("is_with_internet_access", "Yes")
 
-    ai_key = Settings.get_ai_key(chat_bot_type)
+    ai_key = Settings.get_ai_key(chat_bot_model)
 
     text = f"""
     Hey, your bot has been created! 
-    Chatbot type: {chat_bot_type} 
+    Chatbot type: {chat_bot_model} 
     Memory: {is_with_memory} 
     Context: {is_with_context}  
     Internet access: {is_with_internet_access}"""
 
     await message.answer(text=text, reply_markup=ReplyKeyboardRemove())
+
+
+@form_router.message(Command("reset"))
+async def command_reset(message: Message) -> None:
+    """
+    Allow user to reset context
+    """
+    # here some logic
+    await message.answer(
+        "The context has been reset.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.message(Command("uploadPDF"))
+async def command_upload_new_pdf(message: Message) -> None:
+    """
+    Allow user to upload new PDF file
+    """
+    # here some logic
+    await message.answer(
+        "New PDF file has been uploaded.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@form_router.message(Command("sendURL"))
+async def command_send_new_url(message: Message) -> None:
+    """
+    Allow user to send new URL
+    """
+    # here some logic
+    await message.answer(
+        "New URL has been sent.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
